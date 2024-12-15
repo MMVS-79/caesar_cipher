686aa7c0c7ad9b794cc1c65a9e2cbabf3d928045
MMVS <zeinny1997@gmail.com>
2024-12-14 19:24:08 -0700

    adding some comments in the code to explain the logic

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -10,8 +10,8 @@ module CaesarCipher
     message.chars.map do |char|
       if char.match(/[a-z]/)
         # Shift lowercase letters
-        ((char.ord - 'a'.ord + shift) % 26 + 'a'.ord).chr
-      elsif char.match(/[A-Z]/)
+        ((char.ord - 'a'.ord + shift) % 26 + 'a'.ord).chr #% 26 makes sure that after shifting a letter, the result is always a valid letter within the alphabet
+      elsif char.match(/[A-Z]/) #ASCII value of 'z' is 122
         # Shift uppercase letters
         ((char.ord - 'A'.ord + shift) % 26 + 'A'.ord).chr
       else
